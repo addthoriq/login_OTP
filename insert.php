e77525a0c87211926ebccf55be207ebcfc56c160
addthoriq <alvarro.thoriq@gmail.com>
2023-05-11 15:36:37 +0700

    Initial commit

---
+++ insert.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'koneksi.php';
+
+// $sql = "INSERT INTO users VALUES ('00002', 'Kumachan', 'alvarro.thoriq@gmail.com')";
+// $koneksi->query($sql);
+
+header('location: read.php');
---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+
+$server = "localhost";
+$username = "loginOTP";
+$password = "123";
+$dbname = "loginOTP";
+$port = "5432";
+
+$koneksi = new PDO("pgsql:dbname=$dbname;host=$server", $username, $password);
+
+if (!$koneksi) {
+  die("Koneksi gagal");
+}
