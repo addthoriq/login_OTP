d810032f75e89fbd7f1e969ed09e267a20d5f932
addthoriq <alvarro.thoriq@gmail.com>
2023-05-25 23:40:25 +0700

    kelar, login berhasil

--- login.php
+++ login.php
@@ -16,17 +16,22 @@ if (isset($_SESSION['email'])) {
 </head>
 
 <body>
-  <form action="" method="post">
+  <form action="proses_login.php" method="post">
     <span id="email_error" style="color: red;"></span><br>
     <label for="email">Masukkan Email anda:</label>
     <input type="email" name="email" id="email">
     <button type="button" onclick="kirim_otp()" id="btn_kirim_otp">Kirim OTP</button>
+    <br>
+    <label for="kode_otp" id="label_otp">Masukkan Kode OTP</label>
     <input type="text" name="kode_otp" id="kode_otp">
+    <button type="submit" id="btn_submit">Login</button>
   </form>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 <script>
   jQuery('#kode_otp').hide();
+  jQuery('#label_otp').hide();
+  jQuery('#btn_submit').hide();
 
   function kirim_otp() {
     jQuery('#email_error').html('');
@@ -43,9 +48,11 @@ if (isset($_SESSION['email'])) {
         success: (result) => {
           console.log(result);
           if (result == 'done') {
-            jQuery('#email').attr('disabled', true);
+            jQuery('#email').attr('readonly', true);
             jQuery('#btn_kirim_otp').hide();
             jQuery('#kode_otp').show();
+            jQuery('#label_otp').show();
+            jQuery('#btn_submit').show();
           }
         }
       })
--- proses_login.php
+++ proses_login.php
@@ -1,18 +1,37 @@
 <?php
 
-include 'koneksi.php';
+require 'koneksi.php';
+require 'func_generateOTP.php';
 
-session_start();
+// $tipe = get_safe_value($koneksi, $_POST['tipe']);
+$otp = $_POST['kode_otp'];
+$email = get_safe_value($koneksi, $_POST['email']);
 
-$email = $_POST['email'];
-
-$sql = "SELECT * FROM users WHERE email = '$email'";
+$sql = "SELECT * FROM users WHERE email = $email";
 $hasil = $koneksi->query($sql)->fetchObject();
 
-if (!empty($email)) {
+if ($otp == $_SESSION['EMAIL_OTP']) {
+  unset($_SESSION['EMAIL_OTP']);
   $_SESSION['email'] = $hasil->email;
   $_SESSION['name'] = $hasil->name;
   header("location: index.php");
 } else {
-  echo "Email tidak ada";
+  echo "gagal";
 }
+
+// if ($tipe == "'email'") {
+  
+// }
+
+// $email = $_POST['email'];
+
+// $sql = "SELECT * FROM users WHERE email = '$email'";
+// $hasil = $koneksi->query($sql)->fetchObject();
+
+// if (!empty($email)) {
+//   $_SESSION['email'] = $hasil->email;
+//   $_SESSION['name'] = $hasil->name;
+//   header("location: index.php");
+// } else {
+//   echo "Email tidak ada";
+// }
